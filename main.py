ffd7044398b347719d3cc70232c238d0df63ac14
andriw aparecido <andriw1919@gmail.com>
2026-04-20 03:15:50 -0300

    criando rotas de capturade items

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
+mock_items = [
+    {"id": 1, "item": "café 1kg", "price": "14.00"},
+    {"id": 2, "item": "macarrão 300g", "price": "3.50"},
+    {"id": 3, "item": "Feijao 1kg", "price": "5.20"}
+]
 
 class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool | None = None
+    id: int
+    item: str
+    price: str | None = None
 
 
 @app.get("/")
@@ -15,11 +21,16 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/items",response_model=List[Item])
+def get_items():
+    return mock_items
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get("/items/{item_id}", response_model=Item)
+def get_item(item_id: int):
+    item = next((i for i in mock_items if i['id'] == item_id),None)
+    if not item:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+    return item
+    
+
